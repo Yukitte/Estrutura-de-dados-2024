ffe930e5af436f725f0343d78898720d30de3d02
Yukitte <gabribrielnovaes@gmail.com>
2024-03-26 14:51:49 -0300

    Merge branch 'main' of https://github.com/Yukitte/Estrutura-de-dados-2024

--- atividade 2/atividade_2.c
+++ atividade 2/atividade_2.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int argc, int *argv[]){
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Uso: %s <numero>\n", argv[0]);
+        return 1;
+    }
 
-    int primo , i;
-    int numero;
-    // int numero = atoi(argv[-1]);
+    int primo = 0;
+    int numero = atoi(argv[1]);
 
-    for(i=2; i <= numero /2;i++ ){
-        if (numero % i ==0){
-            primo ++;
+    if (numero <= 1) {
+        printf("false\n");
+        return 0;
+    }
+
+    for (int i = 2; i <= numero / 2 + 1; i++) {
+        if (numero % i == 0) {
+            primo = 1;
             break;
         }
     }
 
-    if (numero==primo){
-        printf("true");
-    }else{
-        printf("false");
+    if (primo == 0) {
+        printf("true\n");
+    } else {
+        printf("false\n");
     }
+
     return 0;
 }
